67e5a2c6e202e0482172f381cb7f857b0b10ffe1
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2023-11-23 16:26:54 +0100

    On-going dev

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,83 +1,132 @@
 /**
- * When typescript eslint will support new eslint flat config, we will be able to put this file into a seperate
- * ecmascript module. However requiring it does not work as require implies node context. So impossible to do it
- * for now
+ * When typescript eslint will support new eslint flat config, we will be able to put this file into the config repo.
  */
+/* eslint-disable import/no-commonjs */
+
+const node = true;
+const browser = false;
+const library = false;
+
+/**
+ * @type {import('eslint').Linter.BaseConfig['parser']}
+ */
+const javascriptParser = '@typescript-eslint/parser';
+
+/**
+ * @type {import('eslint').Linter.BaseConfig['settings']}
+ */
+const javascriptSettings = {
+	'import/parsers': {
+		'@typescript-eslint/parser': ['.ts', '.tsx']
+	},
+	'import/resolver': {
+		typescript: {
+			alwaysTryTypes: true
+		}
+	}
+};
 
 /**
  * @type {import('eslint').Linter.Config['rules']}
  */
-const silencedRules = {
+const extraRules = {
 	'@typescript-eslint/no-namespace': 'off', // We want to be able to use namespace
-	'no-redeclare': 'off' // We want to allow types and variables with same names
+	'no-redeclare': 'off', // We want to allow types and variables with same names
+	'codegen/codegen': 'error',
+	'sort-destructure-keys/sort-destructure-keys': 2,
+	'import/first': 'error',
+	'import/newline-after-import': 'error',
+	'import/no-relative-packages': 'error',
+	'import/consistent-type-specifier-style': ['error', 'prefer-inline'],
+	'import/no-extraneous-dependencies': [
+		'error',
+		{
+			devDependencies: false,
+			optionalDependencies: false,
+			peerDependencies: false
+		}
+	],
+	'import/no-import-module-exports': 'error',
+	'import/no-self-import': 'error',
+	'import/extensions': ['error', 'never'],
+	'import/no-empty-named-blocks': 'error',
+	'import/no-amd': 'error',
+	'import/no-commonjs': 'error',
+	'import/no-unresolved': 'error',
+	'import/no-duplicates': 'error'
 };
 
 /**
  * @type {import('eslint').Linter.ParserOptions}
  */
-const javascriptBaseParserOptions = {
+const javascriptParserOptions = {
 	ecmaVersion: 'latest',
-	ecmaFeatures: { impliedStrict: true }
+	ecmaFeatures: { impliedStrict: true },
+	project: true,
+	tsConfigRootDir: './tsconfig.json'
 };
 
 /**
  * @type {import('eslint').Linter.BaseConfig['extends']}
  */
-const javascriptBaseExtends = ['eslint:recommended', 'prettier'];
+const javascriptExtends = [
+	'eslint:recommended',
+	'plugin:import/recommended',
+	'plugin:import/typescript',
+	'plugin:deprecation/recommended',
+	'plugin:@typescript-eslint/recommended-type-checked',
+	'prettier'
+];
+
+/**
+ * @type {import('eslint').Linter.BaseConfig['plugins']}
+ */
+const javascriptPlugins = ['codegen', 'sort-destructure-keys'];
 
 /**
  * @type {import('eslint').Linter.Config}
  */
 module.exports = {
-	// Same rules as .gitignore
+	// Same rules as in .gitignore
 	ignorePatterns: ['dist/'],
 	root: true,
-	// env is set globally because it needs to be extended and it does not disturb non
-	// javascript parsers
-	env: {
-		es2024: true,
-		node: true
-	},
 	overrides: [
 		{
-			files: ['**/*.js', '**/*.mjs', '**/*.cjs', '**/*.ts'],
-			overrides: [
-				{
-					files: ['**/*.js', '**/*.mjs', '**/*.cjs'],
-					extends: javascriptBaseExtends
-				},
-				{
-					files: ['**/*.cjs'],
-					parserOptions: {
-						...javascriptBaseParserOptions,
-						sourceType: 'script'
-					}
-				},
-				{
-					files: ['**/*.js', '**/*.mjs'],
-					parserOptions: {
-						...javascriptBaseParserOptions,
-						sourceType: 'module'
-					}
-				},
-				{
-					files: ['**/*.ts'],
-					// eslint-recommended must come before 'plugin:@typescript-eslint/strict-type-checked'
-					// because the second turns off rules of the first.
-					extends: [
-						'eslint:recommended',
-						'plugin:@typescript-eslint/strict-type-checked',
-						'prettier'
-					],
-					parser: '@typescript-eslint/parser',
-					plugins: ['@typescript-eslint'],
-					parserOptions: { project: true }
-				},
-				{
-					files: ['**/*.js', '**/*.mjs', '**/*.cjs', '**/*.ts'],
-					rules: silencedRules
-				}
-			]
+			files: ['**/*.js', '**/*.mjs', '**/*.ts'],
+			parser: javascriptParser,
+			settings: javascriptSettings,
+			extends: javascriptExtends,
+			parserOptions: {
+				...javascriptParserOptions,
+				sourceType: 'module'
+			},
+			plugins: javascriptPlugins,
+			rules: extraRules,
+			env: {
+				es2024: true,
+				node,
+				browser,
+				['shared-node-browser']: library
+			}
+		},
+		{
+			files: ['**/*.cjs'],
+			parser: javascriptParser,
+			settings: javascriptSettings,
+			extends: javascriptExtends,
+			parserOptions: {
+				...javascriptParserOptions,
+				sourceType: 'script'
+			},
+			plugins: javascriptPlugins,
+			rules: extraRules,
+			env: {
+				es2024: true,
+				commonjs: true,
+				node,
+				browser,
+				['shared-node-browser']: library
+			}
 		},
 		{
 			files: ['**/*.html'],
